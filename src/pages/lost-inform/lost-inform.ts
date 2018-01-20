b5e88384d930f5360e6fb26f66490a5ad633c5ba
DESKTOP-629UB1H\Surface Pro 3 <applefuji1908@gmail.com>
2018-01-20 18:54:13 +0700

    update page lost-inform and page add-dog change value male female in formcontrol

--- src/pages/lost-inform/lost-inform.ts
+++ src/pages/lost-inform/lost-inform.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { LostMainPage} from '../lost-main/lost-main';
+import { IonicPage, NavController, NavParams,LoadingController  } from 'ionic-angular';
+import { LostMainPage } from '../lost-main/lost-main';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { UserServiceProvider } from '../../providers/user-service/user-service'
+import { ImageProvider } from '../../providers/image/image'
+import { DatabaseProvider } from '../../providers/database/database'
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database-deprecated";
 /**
  * Generated class for the LostInformPage page.
@@ -15,55 +19,102 @@ import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/databa
   templateUrl: 'lost-inform.html',
 })
 export class LostInformPage {
-  infoLost = {
-    dogName: '',
-    dogAge: '',
-    breed:'',
-    contactMiss: '',
-    dogDetail: '',
-    dogImage: '',
-    reward: '',
-    status: 'lost'
-  }
-  announceMissing: FirebaseListObservable<any>;
+  private annoucelost: FormGroup;
+  uid: '';
+  dog_image_dataurl: string;
+  uploadedImage: any = null;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFireDatabase) {
-    this.announceMissing = af.list('/announceMissing');
+  constructor(private loadingCtrl: LoadingController, private _DB: DatabaseProvider, private db: AngularFireDatabase, private userService: UserServiceProvider, private image: ImageProvider, private formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams) {
+    this.uid = userService.uid;
+    this.annoucelost = this.formBuilder.group({
+      dogName: ['', Validators.required],
+      breed: ['', Validators.required],
+      gender: ['', Validators.required],
+      age: ['', Validators.required],
+      contactMiss: [''],
+      dogDetail: [''],
+      reward: [''],
+    });
   }
-  infoForm() { 
-    console.dir(this.infoLost);
-    if (this.infoLost.dogImage) {
-      this.uploadImg(this.infoLost.dogImage);     
-    }
-    else { 
-      alert("please select image");
-    }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddMyDogPage');
   }
-  isSelected(e) { 
-    console.log(e);
-    let dogImage:any = e.target.files[0];
-    let reader = new FileReader();
-    reader.onload = (e:any) => { 
-      this.infoLost.dogImage = e.target.result;
+  addAnnounceLost() {
+    if (this.uploadedImage == null) {
+      this.uploadedImage = 'https://firebasestorage.googleapis.com/v0/b/findingmydogs-kmitl.appspot.com/o/dogs%2F8-side.jpg?alt=media&token=c84f0afe-9880-4d8e-82b2-743e5ab92c70';
+    }
+    this.db.database.ref('/announceMissing').push().set({
+      uid: this.uid,
+      dogName: this.annoucelost.value.dogName,
+      breed: this.annoucelost.value.breed,
+      gender: this.annoucelost.value.gender,
+      age: this.annoucelost.value.age,
+      contactMiss: this.annoucelost.value.contactMiss,
+      dogDetail: this.annoucelost.value.dogDetail,
+      reward: this.annoucelost.value.reward,
+      photo: this.uploadedImage,
+    }).then(() => { this.navCtrl.pop() })
+  }
+  getImage() {
+
+    this.dog_image_dataurl = this.image.presentActionSheet(); //ได้ภาพ base64
+
+    this._DB.uploadImageDog(this.dog_image_dataurl) //อัพขึ้นไปบน storage ได้ downloadURL
+      .then((snapshot: any) => {
+        this.uploadedImage = snapshot.downloadURL; //เอา downloadURL มาแสดง
+        console.log(this.uploadedImage)
+      })
+  }
+  // infoLost = {
+  //   dogName: '',
+  //   dogAge: '',
+  //   breed:'',
+  //   contactMiss: '',
+  //   dogDetail: '',
+  //   dogImage: '',
+  //   reward: '',
+  //   status: 'lost'
+  // }
+  // announceMissing: FirebaseListObservable<any>;
+
+  // constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFireDatabase) {
+  //   this.announceMissing = af.list('/announceMissing');
+  // }
+  // infoForm() { 
+  //   console.dir(this.infoLost);
+  //   if (this.infoLost.dogImage) {
+  //     this.uploadImg(this.infoLost.dogImage);     
+  //   }
+  //   else { 
+  //     alert("please select image");
+  //   }
+  // }
+  // isSelected(e) { 
+  //   console.log(e);
+  //   let dogImage:any = e.target.files[0];
+  //   let reader = new FileReader();
+  //   reader.onload = (e:any) => { 
+  //     this.infoLost.dogImage = e.target.result;
       
-    }
-    reader.readAsDataURL(dogImage);
-  }
-  uploadImg(img64) { 
-    // this.announceMissing.push({
-    //   dogName: this.infoLost.dogName,
-    //   dogAge: this.infoLost.dogAge,
-    //   breed:'',
-    //   contactMiss: this.infoLost.contactMiss,
-    //   dogDetail: this.infoLost.dogDetail,
-    //   dogImage: img64,
-    //   reward: this.infoLost.reward,
-    //   status: 'lost'
-    // }).then((data) => { 
-    //   alert("upload success");
-    // })
-  }
-  cancelForm() { 
-    this.navCtrl.push(LostMainPage)
-  }
+  //   }
+  //   reader.readAsDataURL(dogImage);
+  // }
+  // uploadImg(img64) { 
+  //   // this.announceMissing.push({
+  //   //   dogName: this.infoLost.dogName,
+  //   //   dogAge: this.infoLost.dogAge,
+  //   //   breed:'',
+  //   //   contactMiss: this.infoLost.contactMiss,
+  //   //   dogDetail: this.infoLost.dogDetail,
+  //   //   dogImage: img64,
+  //   //   reward: this.infoLost.reward,
+  //   //   status: 'lost'
+  //   // }).then((data) => { 
+  //   //   alert("upload success");
+  //   // })
+  // }
+  // cancelForm() { 
+  //   this.navCtrl.push(LostMainPage)
+  // }
 }
