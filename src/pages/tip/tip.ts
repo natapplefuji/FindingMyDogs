4857f03048cdf27435050d258171ecee0af6564b
JoeMaru <hotdoggrills@hotmail.com>
2018-04-06 17:28:31 +0700

    tip conflict

--- src/pages/tip/tip.ts
+++ src/pages/tip/tip.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
+import { NavController, NavParams, Platform,AlertController } from 'ionic-angular';
 import { PredictProvider } from '../../providers/predict/predict';
 import { LoadingController } from 'ionic-angular';
 import { LostRelatedPage } from '../lost-related/lost-related';
@@ -19,17 +19,23 @@ import { LocationProvider } from '../../providers/location/location'
   templateUrl: 'tip.html',
 })
 export class TipPage {
-  @ViewChild('map') element: ElementRef;
+  // @ViewChild('map') element: ElementRef;
 
-  dogs: [{
-    dogName: string,
-    score: string
-  }];
-  coordinates;
-  uidList = []
-  loc = { lat: 0, lng: 0 };
+  // dogs: [{
+  //   dogName: string,
+  //   score: string
+  // }];
+  // coordinates;
+  // uidList = []
+  // loc = { lat: 0, lng: 0 };
+  breed = ['basenji', 'beagle'];
   announcelistRelate: FirebaseListObservable<any>;
-  constructor(public _loc: LocationProvider, private db: AngularFireDatabase, public googleMaps: GoogleMaps, public navCtrl: NavController, public navParams: NavParams, public _predict: PredictProvider, public loading: LoadingController, public platform: Platform) {
+  breedList: any[];
+  test = {};
+  playerID;
+  constructor(private alertCtrl:AlertController,private plt: Platform,public _loc: LocationProvider, private db: AngularFireDatabase, public googleMaps: GoogleMaps, public navCtrl: NavController, public navParams: NavParams, public _predict: PredictProvider, public loading: LoadingController, public platform: Platform) {
+    
+
     // this.getLocation().then(res => {
     //   console.log("lat is " + res.coords.latitude);
     //   console.log("long is " + res.coords.longitude);
@@ -42,101 +48,157 @@ export class TipPage {
     // console.log("lat : "+this.loc.lat+" long : "+this.loc.lng)
 
 
-    this.announcelistRelate = this.db.list('announceMissing/', {
-      query: {
-        orderByChild: 'breed',
-        equalTo: 'beagle'
-      }, preserveSnapshot: true
-    })
+    // this.announcelistRelate = this.db.list('announceMissing/', {
+    //   query: {
+    //     orderByChild: 'breed',
+    //     equalTo: 'beagle'
+    //   }, preserveSnapshot: true
+    // })
+    // this.getBreed()
   }
-  getList() {
-    /*this.announcelistRelate.subscribe(itemkeys => {
-      itemkeys.forEach(itemkey => {
-        let uid;
-        console.log(itemkey.key);
-        this.db.object('announceMissing/' + itemkey.key).subscribe(user => {
-          uid = user.uid;
-          this.uidList.push(uid);
-
-          console.log(uid);
-        });
-    
-        
-      });
-    })*/
-    var array = []
-    var notificationObj = {
-      contents: { en: "message body" },
-      include_player_ids: [array]
-    };
-    
-    window["plugins"].OneSignal.postNotification(notificationObj,
-      function (successResponse) {
-        alert("Notification Post Success:"+ successResponse);
-      },
-      function (failedResponse) {
-        alert("Notification Post Failed: "+ failedResponse);
-        alert("Notification Post Failed:\n" + JSON.stringify(failedResponse));
-      }
-    );
-  }
-
-  // ionViewDidLoad() {
-  //   console.log('ionViewDidLoad TipPage');
-  //   // this.dog = this._predict.getJsonData();
-  //   // console.log(this.dog)
-  //   this.platform.ready().then(() => {
-  //     this.initMap();
-  //   });
-  // }
-  // getPredict() { 
-  //   this._predict.getJsonData().subscribe((data) => { 
-  //     this.dogs = data
-  //     console.log(this.dogs)
-  //   })
-  // }
-
-
-  initMap() {
-    this._loc.getLocation().then(data => {
-      this.loc.lat = data.coords.latitude;
-      this.loc.lng = data.coords.longitude;
-      console.log("p lat : " + this.loc.lat + " p long : " + this.loc.lng);
-      var mapOptions: GoogleMapOptions = {
-        camera: {
-          target: {
-            lat: this.loc.lat,
-            lng: this.loc.lng
-          },
-          zoom: 18,
-          tilt: 30
-        }
+  testNoti() {
+    window["plugins"].OneSignal.getIds((ids => {
+      this.playerID = ids.userId;
+      var notificationObj = {
+        contents: {
+          en: "Similar Dog was found! Please check..",
+          th: "พบสุนัขลักษณะใกล้เคียง โปรดตรวจสอบ.."
+        },
+        include_player_ids: [this.playerID],
+        //send_after : "March 13th 2018, 3:12:45 pm UTC+07:00"
       };
-      let map: GoogleMap = this.googleMaps.create(this.element.nativeElement, mapOptions);
-
-      map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
-
-        // let markerOptions: MarkerOptions = {
-        //   position: this.loc,
-        //   icon: "assets/images/icons8-Marker-64.png",
-        //   title: 'Our first POI'
-        // };
-
-        // const marker = map.addMarker(markerOptions)
-        //   .then((marker: Marker) => {
-        //     marker.showInfoWindow();
-        //   });
-      })
-    }).catch(err => {
-      console.log(err);
-    })
-
-
+      window["plugins"].OneSignal.postNotification(notificationObj,
+        (successResponse) => {
+          alert("แจ้งแล้วนะ");
+          
+        },
+        (failedResponse) => {
+          //alert("Notification Post Failed playerID ->: " +JSON.stringify(playerIDList));
+          alert(this.playerID)
+          alert("Notification Post Failed:\n" + JSON.stringify(failedResponse));
+        }
+      );
+    }));
+    
+    
+    // this.localNotifications.schedule({
+    //   id: 1,
+    //   title: 'Attention',
+    //   text: 'Simons Notification',
+    //   data: { mydata: 'My hidden message this is' },
+    //   //at: new Date(new Date().getTime() + 5000)
+    // });
   }
+  // getBreed() {
+  //   for (let i = 0; i < this.breed.length; i++){
+  //     this.db.list('announceMissing/', {
+  //       query: {
+  //         orderByChild: 'breed',
+  //         equalTo: this.breed[i]
+  //       }
+  //     }).forEach(element => {
+  //       console.log(element)
+  //       this.test[i] = element;
+  //     })
+  //   }
+    
+    // this.announcelistRelate = this.db.list('announceMissing/', {
+    //   query: {
+    //     orderByChild: 'breed',
+    //     equalTo: 'basenji' || 'beagle'
+    //   }
+    // })
+  // }
 
-  gotoPageRelate() {
-    this.navCtrl.push(LostRelatedPage)
-  }
+
+  // getList() {
+  //   /*this.announcelistRelate.subscribe(itemkeys => {
+  //     itemkeys.forEach(itemkey => {
+  //       let uid;
+  //       console.log(itemkey.key);
+  //       this.db.object('announceMissing/' + itemkey.key).subscribe(user => {
+  //         uid = user.uid;
+  //         this.uidList.push(uid);
+
+  //         console.log(uid);
+  //       });
+
+
+  //     });
+  //   })*/
+  //   var array = []
+  //   var notificationObj = {
+  //     contents: { en: "message body" },
+  //     include_player_ids: [array]
+  //   };
+
+  //   window["plugins"].OneSignal.postNotification(notificationObj,
+  //     function (successResponse) {
+  //       alert("Notification Post Success:"+ successResponse);
+  //     },
+  //     function (failedResponse) {
+  //       alert("Notification Post Failed: "+ failedResponse);
+  //       alert("Notification Post Failed:\n" + JSON.stringify(failedResponse));
+  //     }
+  //   );
+  // }
+
+  // // ionViewDidLoad() {
+  // //   console.log('ionViewDidLoad TipPage');
+  // //   // this.dog = this._predict.getJsonData();
+  // //   // console.log(this.dog)
+  // //   this.platform.ready().then(() => {
+  // //     this.initMap();
+  // //   });
+  // // }
+  // // getPredict() { 
+  // //   this._predict.getJsonData().subscribe((data) => { 
+  // //     this.dogs = data
+  // //     console.log(this.dogs)
+  // //   })
+  // // }
+
+
+  // initMap() {
+  //   this._loc.getLocation().then(data => {
+  //     this.loc.lat = data.coords.latitude;
+  //     this.loc.lng = data.coords.longitude;
+  //     console.log("p lat : " + this.loc.lat + " p long : " + this.loc.lng);
+  //     var mapOptions: GoogleMapOptions = {
+  //       camera: {
+  //         target: {
+  //           lat: this.loc.lat,
+  //           lng: this.loc.lng
+  //         },
+  //         zoom: 18,
+  //         tilt: 30
+  //       }
+  //     };
+  //     let map: GoogleMap = this.googleMaps.create(this.element.nativeElement, mapOptions);
+
+  //     map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
+
+  //       // let markerOptions: MarkerOptions = {
+  //       //   position: this.loc,
+  //       //   icon: "assets/images/icons8-Marker-64.png",
+  //       //   title: 'Our first POI'
+  //       // };
+
+  //       // const marker = map.addMarker(markerOptions)
+  //       //   .then((marker: Marker) => {
+  //       //     marker.showInfoWindow();
+  //       //   });
+  //     })
+  //   }).catch(err => {
+  //     console.log(err);
+  //   })
+
+
+  // }
+
+  // gotoPageRelate() {
+  //   this.navCtrl.push(LostRelatedPage)
+  // }
 
 
 
